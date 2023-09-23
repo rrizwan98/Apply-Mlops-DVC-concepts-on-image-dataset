2a17c670a49bfa422cec75b1d08093c04542a868
email <rrizwan1998@gmail.com>
2023-09-23 15:20:11 +0500

    deploy app on AWS

--- app.py
+++ app.py
@@ -44,10 +44,5 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     # app.run(host='0.0.0.0', port=8080) #local host
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=8080) #for AWS
     # app.run(host='0.0.0.0', port=80) #for AZURE
-=======
-    app.run(host='0.0.0.0', port=8080) #for AWS 
-    # app.run(host='0.0.0.0', port=80) #for AZURE 
->>>>>>> 26ec2d9da4df8d252c82c47fbca0d7c7589e37b8
